61b4f61e606749db9569f6c3f4bad0fe80c46206
chefkuna <sukhyun94@naver.com>
2022-08-21 19:10:41 +0900

    Layout + PageComponent

---
+++ .history/src/app_20220821150002.ts
@@ -0,0 +1,5 @@
+class App {
+  constructor(appRoot: HTMLElement) {
+    
+  }
+}
---
+++ .history/src/app_20220821150017.ts
@@ -0,0 +1,8 @@
+import { PageComponent } from './../.history/src/components/page_20220821145550';
+class App {
+  private readonly page: PageComponent;
+  
+  constructor(appRoot: HTMLElement) {
+
+  }
+}
